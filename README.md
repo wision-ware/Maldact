402dbbe0bb4be5440de52c8f03aace265ba5b994
kavan <vavrinec.kavan@gmail.com>
2024-07-18 18:24:24 +0200

    README.md now includes overview and installation guide, tweaks to setup.py

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-# Maldact
+# Maldact documentation
 
-## Temoporary documentation
+## Overview
 
+This project is an attempt to crate a universal tool to aid scientists with classifying large amounts of data quickly and efficiently. Focusing on simplicity, maintainability, modularity and automation. It features a from scratch custom built feedforward ANN backend that allows for training custom models and then using them on actual data. The project is funded and led by the Department of Space Physics, Institute of Atmospheric Physics, CAS and developed by Vavřinec Kavan, a university student of Computer Science on the Faculty of Mathematics and Physics, CUNI.
+
+## Installation
+
+### Step 1. - Install Anaconda
+
+To install the app, you will firstly need to install Anaconda, preferably the newest version possible.
+
+### Step 2. - Create and activate a Virtual Environment
+
+Then you create a new environment by running: 
+
+```bash
+conda create -n myenv
+```
+
+inside the `Anaconda Prompt`. Then activate it by running 
+
+```bash
+conda activate myenv
+```
+
+Alternatively you can install it into the `base` environment, but that's generally not recommended.
+
+### Step 3. - Download a wheel
+
+You need to download a `.wheel` file from the `dist/` directory of the repository to a known location.
+
+### Step 4. - Install the package
+
+Now simply run 
+
+```bash
+conda install path/to/your/build
+```
+
+then you can verify the installation by running
+
+```bash
+maldact
+```
+
+## Usage
+
+#TODO
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+
 from setuptools import setup, find_packages
+
+# Load the README file for the long description
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='maldact',
@@ -7,22 +12,50 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
+        # List your required dependencies here
+        'numpy',
+        'matplotlib',
+        'pyqt5',
+        'pytest',
+        'cupy'
     ],
     entry_points={
         'console_scripts': [
             'maldact=maldact.main:main'
         ],
     },
-    author='Vavrinec Kavan',
+    author='Vavřinec Kavan',
     author_email='vavrinec.kavan@gmail.com',
     description='Machine learning data classification tool',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/wision-ware/Maldact',
+    project_urls={
+        'Documentation': 'https://github.com/wision-ware/Maldact',
+        'Source': 'https://github.com/wision-ware/Maldact',
+        'Tracker': 'https://github.com/wision-ware/Maldact/issues',
+        'Institute': 'https://www.ufa.cas.cz/en/institute-structure/department-of-space-physics/'
+    },
     classifiers=[
-        'Programming Language :: Python :: 3.11',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
         'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
+    keywords='machine learning, data classification, ANN, GUI, CLI',
     python_requires='>=3.10',
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+    license='MIT',
+    license_files=('LICENSE',),
+    test_suite='tests',
+    maintainer='Department of Space Physics, Institute of Atmospheric Physics, CAS',
+    maintainer_email='ufa@ufa.cas.cz',
 )
